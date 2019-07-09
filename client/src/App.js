d51c06aa49842c1878a548d3e600a8816729dde9
Julia <aseeva.yulia@gmail.com>
2019-07-09 22:20:57 +0200

    make logo linking to dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-grid';
 import { Route, NavLink, HashRouter, Switch } from "react-router-dom";
+import styled from 'styled-components';
 import List from './List';
 import Quiz from './Quiz';
 import Results from './Results';
+import Dashboard from './Dashboard';
+
+const NavTitle = styled.a`
+  text-decoration: none;
+  color: black;
+  width: auto;
+
+  &:hover {
+    color: #AE3F7B;
+  }
+`;
 
 function App () {
     return (
       <HashRouter>
         <Container className="App">
           <Row className="navigation">
-            <Col className="nav-title"><h1>Words Quiz App</h1></Col>
+            <Col className="nav-title"><NavTitle href="/"><h1>Words Quiz App</h1></NavTitle></Col>
             <Col md='auto' className="nav-item"><NavLink to="/quiz">Start Quiz</NavLink></Col>
             <Col md='auto' className="nav-item"><NavLink to="/list">Word List</NavLink></Col>
           </Row>
           <Container className="page-content">
             <Switch>
+              <Route exact path="/" component={Dashboard}/>
               <Route path="/quiz" component={Quiz}/>
               <Route path="/list" component={List}/>
               <Route path="/results" component={Results}/>
---
+++ client/src/Dashboard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const Dashboard = () => {
+    return (
+        <React.Fragment>
+            <div className="dashboard-page">
+                <div className="page-title">
+                    <h1>Last quiz</h1>
+                </div>
+                <div className="charts">
+
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Dashboard;
